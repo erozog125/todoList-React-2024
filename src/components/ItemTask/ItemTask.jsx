ff226a2fb89ffa1d26a2e9c486062cc9d6f1366b
Edwin Rozo - edwindev <erozog125@gmail.com>
2024-03-18 22:29:14 -0500

    Checked tasks

--- src/components/ItemTask/ItemTask.jsx
+++ src/components/ItemTask/ItemTask.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState,useContext } from 'react'
+import { tasksContext } from '../Context/Context'
+
 import './ItemTask.css'
 
 export const ItemTask = ({titleTask,content}) => {
 
+  const context = useContext(tasksContext)
   const [isChecked, setIsChecked] = useState(false)
 
-  const handleCheckboxChange = () => setIsChecked(!isChecked)
+  const handleCheckboxChange = () => {
+    if (isChecked) {
+      context.setDoneTasks(context.doneTasks + 1)
+      // context.setPendingTasks(context.pendingTasks - 1)
+    } else {
+      context.setPendingTasks(context.pendingTasks + 1)
+      // context.setDoneTasks(context.doneTasks - 1)
+    }    
+    setIsChecked(!isChecked)
+  } 
 
   return (
     <li className={ isChecked ? 'item-task checked': 'item-task' }>
